13b83ee82d695eb8a218403870d6ad0b4b6e5920
杨飞 <yangfei86@vip.qq.com>
2023-06-08 19:14:21 +0800

    :bug: fix: fixed some exceptions in shortcut key operation

--- src/hooks/useEditorCanvasKeyPress.ts
+++ src/hooks/useEditorCanvasKeyPress.ts
@@ -1,51 +1,57 @@
 // 仅限编辑器画布使用的快捷键
 import { useKeyPress } from 'ahooks';
 
-import useEditActive from '@/hooks/useEditActive';
 import useSurveyEditor from '@/hooks/useSurveyEditor';
+
+const isEditActive = () => {
+  const activeElemTag = document.activeElement?.tagName;
+  if (activeElemTag) {
+    return ['INPUT', 'TEXTAREA'].includes(activeElemTag);
+  }
+  return false;
+};
 
 const useEditorCanvasKeyPress = () => {
   const {
-    // removeSelectedComponent,
+    removeSelectedComponent,
     copySelectComponent,
     selectPrevComponent,
     selectNextComponent,
     pasteCopiedComponent,
   } = useSurveyEditor();
-  const editActive = useEditActive();
 
   // 复制
   useKeyPress(['ctrl.c', 'meta.c'], () => {
     // 编辑状态不能操作
-    if (editActive) return;
+    if (isEditActive()) return;
     copySelectComponent();
   });
 
   // 粘贴
   useKeyPress(['ctrl.v', 'meta.v'], () => {
     // 编辑状态不能操作
-    if (editActive) return;
+    if (isEditActive()) return;
     pasteCopiedComponent();
   });
 
   // 删除监听
-  // useKeyPress(['backspace', 'delete'], () => {
-  //   // 编辑状态不能操作
-  //   if (editActive) return;
-  //   removeSelectedComponent();
-  // });
+  useKeyPress(['backspace', 'delete'], () => {
+    // 编辑状态不能操作
+    if (isEditActive()) return;
+    removeSelectedComponent();
+  });
 
   // 选中上一个
   useKeyPress('uparrow', () => {
     // 编辑状态不能操作
-    if (editActive) return;
+    if (isEditActive()) return;
     selectPrevComponent();
   });
 
   // 选中下一个
   useKeyPress(40, () => {
     // 编辑状态不能操作
-    if (editActive) return;
+    if (isEditActive()) return;
     selectNextComponent();
   });
 };
